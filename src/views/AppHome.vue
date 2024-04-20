2a8b258391475527daaac1fc029a739998aa0e31
Stefan Seifert <stefanseifert@users.noreply.github.com>
2024-04-20 14:06:01 +0200

    add bgg thread link

--- src/views/AppHome.vue
+++ src/views/AppHome.vue
@@ -11,13 +11,11 @@
   <p v-html="t('home.play1')"></p>
   <p v-html="t('home.play2')"></p>
 
-<!--
   <p class="text-muted fst-italic">
     {{t('home.feedback')}}
-    <a href="https://boardgamegeek.com/thread/XXX" target="_blank" rel="noopener">Board Game Geek Thread</a> |
+    <a href="https://boardgamegeek.com/thread/3287125/brdgmme-welcome-to-the-moon-solo-helper-web-applic" target="_blank" rel="noopener">Board Game Geek Thread</a> |
     <a href="https://github.com/brdgm/welcome-to-the-moon-solo-helper" target="_blank" rel="noopener">GitHub</a>
   </p>
--->
 
   <router-link to="/setupGame" class="btn btn-primary btn-lg mt-3">{{t('action.playGame')}}</router-link>
 
